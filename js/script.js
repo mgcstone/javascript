24a7c081452a4e36fa6d14b798beb538a4da1dbf
Ruslan <example@mail.ru>
2021-05-25 15:08:45 +0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// "use strict";
+// const obj = {
+//     name: "Ruslan",
+//     age: 30,
+//     isMarried: true,
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'apple.jpg', 'orange.gif'];
+// console.log(arr[1]);
+// Урок 7
+// const answers = [];
+
+// answers[0] = prompt('Ваше имя?');
+// answers[1] = prompt('Ваша фамилия?');
+// answers[2] = prompt('Ваш возраст?');
+
+// document.write(answers);
+//Урок 8
+// "use strict";
+
+// const category = 'toys';
+// console.log(`http://someurl.com/${category}`);
+//Урок 9
+"use strict";
